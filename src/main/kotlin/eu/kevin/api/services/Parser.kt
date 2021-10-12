5c671d34411b0e8b5067248f3ae53e7880b229a1
Grantas Gadliauskas <grantas.gadliauskas@kevin.eu>
2021-10-12 19:41:25 +0300

    MOBILE-236: Undo delete parser

---
+++ src/main/kotlin/eu/kevin/api/services/Parser.kt
@@ -0,0 +1,32 @@
+package eu.kevin.api.services
+
+import eu.kevin.api.Dependencies
+import eu.kevin.api.models.payment.initiatePayment.webhook.BankPaymentWebhookPayload
+import eu.kevin.api.models.payment.initiatePayment.webhook.CardPaymentWebhookPayload
+import eu.kevin.api.models.payment.initiatePayment.webhook.HybridPaymentWebhookPayload
+import eu.kevin.api.models.payment.initiatePaymentRefund.webhook.PaymentRefundWebhookPayload
+import kotlinx.serialization.json.Json
+
+class Parser internal constructor(
+    private val serializer: Json
+) {
+    constructor() : this(
+        serializer = Dependencies.serializer
+    )
+
+    fun parseBankPaymentWebhookRequest(request: String): BankPaymentWebhookPayload {
+        return serializer.decodeFromString(BankPaymentWebhookPayload.serializer(), request)
+    }
+
+    fun parseCardPaymentWebhookRequest(request: String): CardPaymentWebhookPayload {
+        return serializer.decodeFromString(CardPaymentWebhookPayload.serializer(), request)
+    }
+
+    fun parseHybridPaymentWebhookRequest(request: String): HybridPaymentWebhookPayload {
+        return serializer.decodeFromString(HybridPaymentWebhookPayload.serializer(), request)
+    }
+
+    fun parsePaymentRefundWebhookRequest(request: String): PaymentRefundWebhookPayload {
+        return serializer.decodeFromString(PaymentRefundWebhookPayload.serializer(), request)
+    }
+}
