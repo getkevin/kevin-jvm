6677e342e04e1eeeae008133e1a6857d5cec5c48
Luca Rospocher <luca.rospocher@kevin.eu>
2022-05-11 12:03:45 +0200

    Add PAYMENT_POS scope related parameters to authentication

--- src/main/kotlin/eu/kevin/api/models/auth/authentication/request/StartAuthenticationRequest.kt
+++ src/main/kotlin/eu/kevin/api/models/auth/authentication/request/StartAuthenticationRequest.kt
@@ -3,6 +3,7 @@ package eu.kevin.api.models.auth.authentication.request
 data class StartAuthenticationRequest @JvmOverloads constructor(
     val requestId: String,
     val redirectUrl: String,
+    var webhookUrl: String? = null, // Needed just if AuthenticationScopes.PAYMENT_POS is present
     var bankId: String? = null,
     var redirectPreferred: Boolean? = null,
     var scopes: List<AuthenticationScopes>? = null,
