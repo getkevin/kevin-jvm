b3f8362a4da5b7524ee07f88e82ae1885c594a63
Luca Rospocher <luca.rospocher@kevin.eu>
2022-06-14 15:48:40 +0200

    Revert "Let datetime be specified in get transactions query params"
    This reverts commit e9107eaff2cd2182eac503e9f7e5766cc48fec7e.

--- src/main/kotlin/eu/kevin/api/serializers/LocalDateTimeSerializer.kt
+++ src/main/kotlin/eu/kevin/api/serializers/LocalDateTimeSerializer.kt
@@ -5,26 +5,19 @@ import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeParseException
 
 @OptIn(ExperimentalSerializationApi::class)
 @Serializer(forClass = LocalDateTime::class)
 object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
-    private val dateTimeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-    private val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    private val format = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
 
     override fun serialize(encoder: Encoder, value: LocalDateTime) {
-        encoder.encodeString(value.format(dateTimeFormat))
+        encoder.encodeString(value.format(format))
     }
 
     override fun deserialize(decoder: Decoder): LocalDateTime {
-        return try {
-            LocalDateTime.parse(decoder.decodeString(), dateTimeFormat)
-        } catch (exception: DateTimeParseException) {
-            LocalDate.parse(decoder.decodeString(), dateFormat).atStartOfDay()
-        }
+        return LocalDateTime.parse(decoder.decodeString(), format)
     }
 }
