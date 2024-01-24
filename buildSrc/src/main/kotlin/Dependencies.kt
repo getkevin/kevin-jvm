5985eea2a287ed2b35b62d6e060c6ebd9b5e8bd5
Kamil Przybylo <kamil.przybylo@kevin.eu>
2024-01-24 13:19:10 +0100

    chore: bump ktor to latest minor version update

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val KOTLIN = "1.6.10"
-    const val KTOR = "2.2.4"
+    const val KTOR = "2.3.7"
     const val KTLINT = "10.2.0"
     const val NEXUS_PUBLISH = "1.1.0"
 }
